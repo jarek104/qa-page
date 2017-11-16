76d3fc34efa2dd225f9afbb461d67edec0f77219
Jerry Olewniczak <jolewniczak@OnBase.net>
2017-11-16 17:07:03 -0500

    im able to capture the data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Winvm } from './data models/winvm';
+
 
 @Component({
   selector: 'qa-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'qa';
+
+  wvmCollection: AngularFirestoreCollection<Winvm>;
+  wvms: Observable<Winvm[]>;
+  constructor(private afs: AngularFirestore) {
+
+  }
+  ngOnInit() {
+    this.wvmCollection = this.afs.collection('WinWms');
+    this.wvms = this.wvmCollection.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,17 @@ import { VmsService } from './services/vms-service';
 import { BrowsersComponent } from './browsers/browsers.component';
 import { BrowsersEditComponent } from './browsers/browsers-edit.component';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDB9ZpAnBy6rwA7-7VpvqxhBnV06qGaqx0',
+  authDomain: 'qa-page-38d95.firebaseapp.com',
+  databaseURL: 'https://qa-page-38d95.firebaseio.com',
+  projectId: 'qa-page-38d95',
+  storageBucket: 'qa-page-38d95.appspot.com',
+  messagingSenderId: '891773206618'
+};
 
 @NgModule({
   declarations: [
@@ -25,7 +36,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [VmsService],
   bootstrap: [AppComponent]
