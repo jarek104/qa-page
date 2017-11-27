2a3e20dbee5fa2072ef5e2c2e935fa6b8be228cd
Jerry Olewniczak <jolewniczak@OnBase.net>
2017-11-27 17:08:11 -0500

    well i have 2 tests but the     browser.ignoreSynchronization = true; is weird

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,23 @@
-import { AppPage } from './app.po';
+import { NgHub } from './app.po';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 describe('qa-page App', () => {
-  let page: AppPage;
+  let page: NgHub;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new NgHub();
+
+  });
+
+  it('should display WFH welcome message', async () => {
+    page.navigateToWFH();
+
+    expect(page.getWFHHeaderElement('qa-work-from-home').getText()).toEqual('Just kidding!');
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to qa!');
+    browser.ignoreSynchronization = true;
+    expect(page.getHeaderElement().getText()).toEqual('Welcome to my app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,20 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
-export class AppPage {
+export class NgHub {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/#/vms');
   }
+  navigateToWFH() {
+    return browser.get('/#/wfh');
+  }
+  getHeaderElement(): ElementFinder {
 
-  getParagraphText() {
-    return element(by.css('qa-root h1')).getText();
+    const el = element(by.css('h1'));
+    // browser.ignoreSynchronization = false;
+    return el;
+  }
+  getWFHHeaderElement(comp: string): ElementFinder {
+    const el = element(by.css(comp + ' h1'));
+    return el;
   }
 }
